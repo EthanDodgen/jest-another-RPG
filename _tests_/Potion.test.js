0929b9db872ce6b0c8f9cab32c18638700e67abf
Fival <thefivalco@gmail.com>
2021-02-09 21:38:09 -0600

    update potion

--- _tests_/Potion.test.js
+++ _tests_/Potion.test.js
@@ -1,9 +1,7 @@
-//const { expect } = require("@jest/globals")
-
-const Potion = require("../lib/Potion.js")
+const Potion = require("../Potion.js")
 
 test("creates a random potion object", () => {
-    const potion = new Potion("")
+    const potion = new Potion()
 
     expect(potion.name).toEqual(expect.any(String))
     expect(potion.name.length).toBeGreaterThan(0)
